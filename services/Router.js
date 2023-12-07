bbc7085e59f9f870bee945273b06716f6ce91f6b
Saif Ali Shaik <saifshine7@gmail.com>
2023-12-07 12:03:10 +0530

    📦 NEW: implement router

--- services/Router.js
+++ services/Router.js
@@ -3,12 +3,40 @@ const Router = {
     document.querySelectorAll("a.navlink").forEach((a) => {
       a.addEventListener("click", function (event) {
         event.preventDefault();
-        console.log("Link Clicked");
+        const url = event.target.getAttribute("href");
+        Router.go(url);
       });
     });
+    // Event handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
+    Router.go(location.pathname);
   },
   go: (route, addToHistory = true) => {
     console.log(`Going to ${route}`);
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Home";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Order";
+        break;
+    }
+    if (pageElement) {
+      // document.querySelector("main").children[0].remove();
+      document.querySelector("main").innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
   },
 };
 
